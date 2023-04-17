ea106bb106a918bd889830ab94603648ac2f19f8
vicenchavez17 <75546835+vicenchavez17@users.noreply.github.com>
2023-04-17 14:50:44 -0700

    Update Server.java
    Created main for Server (Test push)

--- CS401Group4Project/src/Server.java
+++ CS401Group4Project/src/Server.java
@@ -1,4 +1,13 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 
 public class Server {
+    public static void main(String[] args) {
 
+    }
 }
