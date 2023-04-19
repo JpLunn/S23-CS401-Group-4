2a3453ad0c56cd17eddb7639384b6b6d3b4cf88d
Xinye Zhang <89996061+Donielzhong@users.noreply.github.com>
2023-04-19 15:09:02 -0700

    Added basic client code

--- CS401Group4Project/src/Client.java
+++ CS401Group4Project/src/Client.java
@@ -1,4 +1,29 @@
+import java.io.*;
+import java.net.*;
 
 public class Client {
-
+    public static void main(String[] args) {
+        try {
+            // Create a socket to connect to the server on port 8000
+            Socket socket = new Socket("localhost", 8000);
+            
+            // Get the input and output streams for the socket
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            
+            // Send a message to the server
+            out.println("Hello from the client");
+            
+            // Read the server's response
+            String response = in.readLine();
+            System.out.println("Received from server: " + response);
+            
+            // Close the input, output, and socket
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
