b922dac9d213b00eb5962613f8d7026fdf96bad6
vicenchavez17 <75546835+vicenchavez17@users.noreply.github.com>
2023-04-19 17:43:06 -0700

    adding Server attributes

--- CS401Group4Project/src/Server.java
+++ CS401Group4Project/src/Server.java
@@ -7,17 +7,25 @@ import java.util.Queue;
 
 public class Server {
     public static void main(String[] args) {
+        ArrayList<User> userList = new ArrayList<User>;
+        ServerSocket server = null;
+        
         try {
             // Create a ServerSocket to listen for incoming connections on port 8000
-            ServerSocket serverSocket = new ServerSocket(8000);
+            server = new ServerSocket(8000);
+            server.setReuseAddress(true);
+            System.out.println("ServerSocket awaiting connections8...");
             
             while (true) {
-                // Wait for a client to connect
-                Socket clientSocket = serverSocket.accept();
+                Socket client = server.accept();
                 
-                // Create a new thread to handle the client's connection
-                Thread thread = new Thread(new ClientHandler(clientSocket));
-                thread.start();
+                System.out.println("New client connected" + client.getInetAddress().getHostAddress());
+                System.out.println("Connection from " + client + "!");
+                
+                // assigns the client handler to a client socket
+                ClientHandler clientSock = new ClientHandler(client);
+                
+                new Thread(clientSock).start();
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
@@ -27,13 +35,19 @@ public class Server {
 
 class ClientHandler implements Runnable {
     private Socket clientSocket;
+    private final Socket clientSocket;
+    private boolean loggedIn = false; // checks if client has logged in
+    private boolean endConnection = false; // checks if user wants to close connection
+    private Queue<Message> msgQueue; // queue for messages 
     
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
+        msgQueue = new LinkedList<Message>();
     }
     
     public void run() {
         try {
+            
             // Get the input and output streams for the client socket
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
