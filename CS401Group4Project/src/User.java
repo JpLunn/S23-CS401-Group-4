1e4bde095ed6b781c9397ba43f80ea383e51b4e7
vicenchavez17 <75546835+vicenchavez17@users.noreply.github.com>
2023-04-22 11:29:07 -0700

    Setters and getters for User class

--- CS401Group4Project/src/User.java
+++ CS401Group4Project/src/User.java
@@ -2,20 +2,106 @@ import java.util.ArrayList;
 
 public class User {
     private static int counter =0;
-    private UserType userType;
     private int id;
+    private UserType userType;
     private String firstName;
     private String lastName;
-    private String userName;
+    private String username;
     private String password;
     private boolean blockedFlag;
     private UserState userState;
     private ArrayList<MessageThread> threadList = new ArrayList<MessageThread>();
 
-    // User Constructor
-    public User() {
-        
+    // User Constructor for new user
+    public User(UserType userType, String first, String last, String username, String password, boolean blocked, UserState userState, ArrayList<MessageThread> threadList) {
+        this.id = ++counter;
+        this.userType = userType;
+        this.firstName = first;
+        this.lastName = last;
+        this.username = username;
+        this.password = password;
+        this.blockedFlag = blocked;
+        this.userState = userState;
+        this.threadList = threadList;
+    }
+    
+    // User Constructor for loading User
+    public User(int id,UserType userType, String first, String last, String username, String password, boolean blocked, UserState userState, ArrayList<MessageThread> threadList) {
+        this.id = id;
+        this.userType = userType;
+        this.firstName = first;
+        this.lastName = last;
+        this.username = username;
+        this.password = password;
+        this.blockedFlag = blocked;
+        this.userState = userState;
+        this.threadList = threadList;
+    }
+    
+//    Setters
+    public void setID(int id) {
+        this.id = id;
+    }
+    
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+    
+    public void setFirstName(String first) {
+        this.firstName = first;
+    }
+    
+    public void setLastName(String last) {
+        this.lastName = last;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-
+    public void setBlockedFlag(boolean blocked) {
+        this.blockedFlag = blocked;
+    }
+    
+    public void setThreadList(ArrayList<MessageThread> threadList) {
+        this.threadList = threadList;
+    }
+    
+//    Getters
+    public int getID() {
+        return this.id;
+    }
+    
+    public UserType getUserType() {
+        return this.userType;
+    }
+    
+    public String getFirstName() {
+        return this.firstName;
+    }
+    
+    public String getLastName() {
+        return this.lastName;
+    }
+    
+    public String getUsername() {
+        return this.username;
+    }
+    
+    public String getPassword() {
+        return this.password;    
+    }
+    
+    public boolean getBlockedFlag() {
+        return this.blockedFlag;
+    }
+    
+    public ArrayList<MessageThread> getThreadList() {
+        return this.threadList;
+    }
+    
 }
