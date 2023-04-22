a1f9d672be571488ad8c7d0436957facad432e27
vicenchavez17 <75546835+vicenchavez17@users.noreply.github.com>
2023-04-22 09:23:54 -0700

    Added attributes for User class

--- CS401Group4Project/src/User.java
+++ CS401Group4Project/src/User.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 
 public class User {
+    private static int counter =0;
+    private UserType userType;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean blockedFlag;
+    private UserState userState;
+    private ArrayList<MessageThread> threadList = new ArrayList<MessageThread>();
+
+    // User Constructor
+    public User() {
+        
+    }
+
 
 }
