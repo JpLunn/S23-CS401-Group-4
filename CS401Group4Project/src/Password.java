8662b8d11b75377af23b7bc068c2fbf67f243c99
JpLunn <Jon93lunn@sbcglobal.net>
2023-04-23 20:49:22 -0700

    Initial Implementation of Password Class

--- CS401Group4Project/src/Password.java
+++ CS401Group4Project/src/Password.java
@@ -1,4 +1,21 @@
 
 public class Password {
-
+	private String password;
+	private int length;
+	private String[] validCharacters;
+	
+	
+	public Password(String pw) {
+		password = pw;
+	}
+	
+	public bool checkPassword(String pw) {
+		if(password.contentEquals(pw)) {
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	
 }
