7efee6233bc971f458172b30b00e2dadd2161f7f
JpLunn <Jon93lunn@sbcglobal.net>
2023-04-25 22:26:54 -0700

    GUI Imports and Skeleton functions from UML

--- CS401Group4Project/src/GUI.java
+++ CS401Group4Project/src/GUI.java
@@ -1,4 +1,58 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class GUI {
 	//this is just a test - Charles
+	private User activeUser= null;
+	
+	
+	public GUI(User loginUser) {
+		activeUser = loginUser;
+	}
+	
+	private void createMessage() {
+		
+	}
+	
+	private void createMessageLoc() {
+		
+	}
+	
+	public void login() {
+		
+	}
+	
+	private void logout() {
+		
+	}
+	
+	private void processCommands() {
+		
+	}
+	
+	private void sendMessage() {
+		
+	}
+	
+	private void viewOnlineUsers() {
+		
+	}
+	
+	private void viewITLogs() {
+		
+	}
+	
+	private void viewMessages() {
+		
+	}
+
 }
