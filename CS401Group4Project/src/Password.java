a64adcde2259001cc093a917410005017a70d326
Xinye Zhang <89996061+Donielzhong@users.noreply.github.com>
2023-04-26 14:28:55 -0700

    Added Password hashing and verify fxs

--- CS401Group4Project/src/Password.java
+++ CS401Group4Project/src/Password.java
@@ -1,21 +1,34 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Password {
-	private String password;
-	private int length;
-	private String[] validCharacters;
-	
-	
-	public Password(String pw) {
-		password = pw;
-	}
-	
-	public bool checkPassword(String pw) {
-		if(password.contentEquals(pw)) {
-			return true;
-		}
-		else
-			return false;
-	}
-	
-	
+    private static final String SALT = "yourSaltHere";
+    private String passwordHash;
+
+    public Password(String password) {
+        passwordHash = generateHash(password);
+    }
+
+    public boolean verify(String password) {
+        String hash = generateHash(password);
+        return hash.equals(passwordHash);
+    }
+
+    private String generateHash(String password) {
+        String saltedPassword = SALT + password;
+        String hash = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(saltedPassword.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            }
+            hash = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hash;
+    }
 }
