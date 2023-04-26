31aa7cba24a4460758d302411f8a9e602ebdb0ad
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2023-04-26 14:00:31 -0700

    Update Message.java
    Added Attributes and constructors

--- CS401Group4Project/src/Message.java
+++ CS401Group4Project/src/Message.java
@@ -1,4 +1,28 @@
+import java.util.String;
 
 public class Message {
-
+	private int counter;
+	private int id;
+	private MessageType type;
+	private String content;
+	private User owner;
+	private Date created;
+	private Boolean editedFlag;
+	private Date dateEdited;
+	private int messageThreadID
+	
+	public Message(Message type) {
+		this.type = type;
+	}
+	
+	public Message(User owner, String content, MessageType type) {
+		this.owner = owner;
+		this.content = content;
+		this.type = type;
+	}
+	
+	public Message(User owner, String content, ChatLog chat) {
+		this.owner = owner;
+		this.content = content;
+	}
 }
