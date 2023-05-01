bbf00aae11d4708f81494c4c174d3d6a502eb27d
vswansonoswood <123413601+vswansonoswood@users.noreply.github.com>
2023-05-01 01:37:55 -0700

    Fixed Queue
    Fixed Queue Declaration

--- CS401Group4Project/src/Client.java
+++ CS401Group4Project/src/Client.java
@@ -9,7 +9,7 @@ public class Client {
 	private User currentUser;
 	//private ListArray<Notifications> notifications;
 	private boolean SessionActive = false;
-	private Queue<Message> messageQueue = new LinkedList();
+	private Queue<Message> messageQueue = new LinkedList<>();
 	
 	
 	
