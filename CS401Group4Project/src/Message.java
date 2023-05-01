6f97cfdf0585853239e0b8730c731fc1a9dd5158
Xinye Zhang <89996061+Donielzhong@users.noreply.github.com>
2023-04-30 22:07:54 -0700

    Added Constructors/Setters & changed MsgType
    testing: instead of using a separate class for message type, use as string within class.

--- CS401Group4Project/src/Message.java
+++ CS401Group4Project/src/Message.java
@@ -5,6 +5,7 @@ public class Message implements Serializable {
 	private int counter;
 	private int id;
 	private MessageType type;
+	private String MsgType; //Testing out msgType with string instead of class
 	private String content;
 	private User owner;
 	private Date created;
@@ -12,39 +13,68 @@ public class Message implements Serializable {
 	private Date dateEdited;
 	private int messageThreadID = -1;
 	
-	public Message(MessageType type) {
-		this.type = type;
+	//Constructor
+	public Message() {
+		this.setType("NONE");
+		this.content = ("");
+		this.setOwner(new User());
+		this.setCreatedDate(new Date());
+		
+	}
+	public Message(String MsgType) {
+		this.MsgType = MsgType;
+		this.content = ("");
+		this.setOwner(new User());
+		this.setCreatedDate(new Date());
 	}
 	
-	public Message(User owner, String content, MessageType type) {
-		this.owner = owner;
-		this.content = content;
-		this.type = type;
+	public Message(User owner, String content, String MsgType) {
+		this.setOwner(owner);
+		this.setContent(content);
+		this.setType(MsgType);
+		this.setCreatedDate(new Date());
 	}
 	
 	public Message(User owner, String content, int threadID) {
         this.owner = owner;
         this.messageThreadID = Integer.valueOf(threadID);
         this.type = MessageType.NEW_TEXT;
+        this.setCreatedDate(new Date());
     }
 	
 	public Message(User owner, String content, MessageThread chat) {
 		this.owner = owner;
 		this.content = content;
+		this.setCreatedDate(new Date());
+	}
+	
+	public void setType(String MsgType) {
+		this.MsgType = MsgType;
 	}
 
-	public MessageType getType() {
-		return this.type;
+	public String getType() {
+		return this.MsgType;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
 	}
 	
 	public String getContent() {
 		return content;
 	}
 	
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
 	public User getOwner() { //Owner is the Sender
 		return owner;
 	}
 	
+	public void setCreatedDate(Date dateEdited) {
+		this.dateEdited = dateEdited;
+		
+	}
 	public Date getCreationDate() {
 		return created;
 	}
@@ -59,5 +89,4 @@ public class Message implements Serializable {
 		}
 	}
 	
-//	Comment test
 }
