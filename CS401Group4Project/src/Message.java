6303d819adb9ced1376caa70a312f091b236ed61
Xinye Zhang <89996061+Donielzhong@users.noreply.github.com>
2023-04-30 23:34:35 -0700

    Added setters and getters

--- CS401Group4Project/src/Message.java
+++ CS401Group4Project/src/Message.java
@@ -11,76 +11,95 @@ public class Message implements Serializable {
 	private Boolean editedFlag;
 	private Date dateEdited;
 	private int messageThreadID = -1;
-	
 
-	
-   public Message(MessageType MsgType) {
-        this.type = MsgType;
-        this.content = ("");
-        this.setCreationDate(new Date());
-   }
-	
+	public Message(MessageType MsgType) {
+		this.type = MsgType;
+		this.content = ("");
+		this.setCreatedDate(new Date());
+	}
+
 	public Message(User owner, String content, MessageType type) {
 		this.owner = owner;
 		this.content = content;
 		this.type = type;
+		this.setCreatedDate(new Date());
 	}
+
 	public Message(User owner, MessageType type) {
-        this.setOwner(owner);
-        this.setType(type);
-        this.setCreationDate(new Date());
-    }
-	
+		this.setOwner(owner);
+		this.setType(type);
+		this.setCreatedDate(new Date());
+	}
+
 	public Message(User owner, String content, int threadID) {
-        this.owner = owner;
-        this.messageThreadID = Integer.valueOf(threadID);
-        this.type = MessageType.NEW_TEXT;
-    }
-	
+		this.owner = owner;
+		this.messageThreadID = Integer.valueOf(threadID);
+		this.type = MessageType.NEW_TEXT;
+		this.setCreatedDate(new Date());
+	}
+
 	public Message(User owner, String content, MessageThread chat) {
 		this.owner = owner;
 		this.content = content;
+		this.setCreatedDate(new Date());
 	}
 
-   public void setType(MessageType MsgType) {
-        this.type = MsgType;
-    }
+	public void setType(MessageType MsgType) {
+		this.type = MsgType;
+	}
 
 	public MessageType getType() {
 		return this.type;
 	}
-	
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
 	public String getContent() {
 		return content;
 	}
-	
-	public User getOwner() { //Owner is the Sender
+
+	public User getOwner() {
 		return owner;
 	}
-   public void setOwner(User user) { //Owner is the Sender
-        this.owner = user;
-    }
-	
-	public Date getCreationDate() {
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public Date getCreatedDate() {
 		return created;
 	}
-	public void setCreationDate(Date date) {
-        this.created = date;
-    }
-	
+
+	public void setCreatedDate(Date created) {
+		this.created = created;
+	}
+
+	public Boolean getEditedFlag() {
+		return editedFlag;
+	}
+
+	public void setEditedFlag(Boolean editedFlag) {
+		this.editedFlag = editedFlag;
+	}
+
+	public Date getDateEdited() {
+		return dateEdited;
+	}
+
+	public void setDateEdited(Date dateEdited) {
+		this.dateEdited = dateEdited;
+	}
+
 	public int getMessageThreadID() {
 		return messageThreadID;
 	}
-	
+
 	public void setMessageThreadID(int givenID) {
-		if(messageThreadID == -1) {
+		if (messageThreadID == -1) {
 			messageThreadID = givenID;
 		}
 	}
-	
-//	Comment test
+
 }
